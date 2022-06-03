5bae019e02f890f57ba61d57ae475f652736eace
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2022-06-03 15:47:35 +0200

    Start
    Lagt in dependen. för nedb-promise , express , samt kriterier

---
+++ app.js
@@ -0,0 +1,34 @@
+/**
+ * Kunna hämta kaffe-meny (se menu.json nedanför som ni ska använda och skicka tillbaka som svar till frontend)
+ * Kunna lägga till beställningar (dessa beställningar ska kunna koppla mot ett konto se nedan men det ska även gå att lägga beställning utan ett konto som en gäst)
+ * Ska kunna lägga till konton (användarnamn och lösenord) och koppla ett konto till de beställningar som görs (för att kunna se orderhistorik)
+ * Se orderhistorik
+ * Allt ska sparas i en NeDB-databas
+ */
+
+/**
+ * ENDOINTS
+ * 
+ * GET /api/menu                - Hämta kaffe-meny
+ * POST /api/order              - Ska kunna spara Kaffe beställning returnera ETA,Ordernr (kan slumpas), Ev anv namn kan kopplas till best
+ * GET /api/order/:id           - Returnera orderhistorik för en specifik användare
+ * POST /api/account/signup     - Ska kunna skapa ett användar konto
+ * POST /api/account/login      - Ska kunna logga in
+ */
+
+
+
+
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
